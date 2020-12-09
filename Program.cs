0e9d9c1fc6ef79689fa0526ff04caeeb88793cf3
Joshua Johanning <jjohanning@10thmagnitude.com>
2020-12-09 13:20:27 -0600

    initial commit of console app

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace zero_to_hero_codeql_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
